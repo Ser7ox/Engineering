0eb7cf50cd3a02a74595bea40c0b4fbd6e721650
Ser7ox <fantasiaflavio@gmail.com>
2021-11-19 14:59:40 +0100

    onchange configured

--- angular-rubrica/src/app/form/form.component.ts
+++ angular-rubrica/src/app/form/form.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { Persona } from '../persona';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,20 +8,37 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
   @Input() FormPerson: Persona;
-
-  profilo = this.fb.group({
-    nome: [''],
-    cognome: [''],
-    sesso: [''],
-    indirizzo: [''],
-    telefono: [''],
-  });
   
-
   constructor(private fb: FormBuilder) { }
+  
+  profilo: FormGroup;
 
   ngOnInit(): void {
+    this.profilo = this.fb.group({
+      nome: [undefined, Validators.compose([Validators.minLength(3), Validators.maxLength(20), Validators.required])],
+      cognome: [undefined, Validators.compose([Validators.minLength(3), Validators.maxLength(20), Validators.required])],
+      sesso: [undefined],
+      telefono: [undefined],
+      indirizzo: [undefined],   
+    });
+  
+    this.setProfilo();
+    
   }
+
+  setProfilo() {
+      this.profilo.get('nome').setValue(this.FormPerson?.nome);
+      this.profilo.get('cognome').setValue(this.FormPerson?.cognome);
+      this.profilo.get('sesso').setValue(this.FormPerson?.sesso);
+      this.profilo.get('telefono').setValue(this.FormPerson?.telefono);
+      this.profilo.get('indirizzo').setValue(this.FormPerson?.indirizzo);
+    }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!changes.FormPerson.firstChange) {
+      this.setProfilo();
+    }
+    }
+
 }
