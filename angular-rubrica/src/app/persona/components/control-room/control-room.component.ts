41c157f92d3a02a85bbb246b1ccd0f66ed2b9625
Ser7ox <fantasiaflavio@gmail.com>
2022-01-17 17:22:07 +0100

    console log lengthPersona with set and get @Input()

--- angular-rubrica/src/app/persona/components/control-room/control-room.component.ts
+++ angular-rubrica/src/app/persona/components/control-room/control-room.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,17 +8,26 @@ import { Router } from '@angular/router';
 })
 export class ControlRoomComponent implements OnInit {
 
-  @Input() lengthPersona: number;
+  private _lengthPersona: number;
   showLoad = true;
+
+  @Input() set lunghezza(lengthPersona:number) {
+    this._lengthPersona = lengthPersona;
+    console.log(this._lengthPersona);
+  }
+
+  get lunghezza(): number {
+    return this._lengthPersona;
+  }
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log("Counter of users: " + changes.lengthPersona.currentValue);
-  }
+  //ngOnChanges(changes: SimpleChanges) {
+  //  console.log("Counter of users: " + changes.lengthPersona.currentValue);
+  //}
 
   form(){this.router.navigate(['persona/form']);}
 
