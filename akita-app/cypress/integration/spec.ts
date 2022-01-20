037289d835b45d2bd34a17c9c1569151ade5b09a
Ser7ox <fantasiaflavio@gmail.com>
2022-01-20 12:23:02 +0100

    cypress test

---
+++ akita-app/cypress/integration/spec.ts
@@ -0,0 +1,43 @@
+describe('My First Test', () => {
+  const expectedTitle = 'akita-app'
+  it('Visits the initial project page', () => {
+    cy.visit('/home/table')
+    cy.contains('Actions')
+  })
+
+  it('Should be true', () => {
+    expect(true).to.equal(true)
+  })
+
+  it('Site must contains the term User Card', () => {
+    cy.contains('User card')
+  })
+
+  it('Should contain expected title', () => {
+    cy.contains(expectedTitle);
+    //cy.get('[data-test=button]').click();
+  })
+
+  it('Visit user card', () => {
+    cy.visit('http://localhost:4200/home/card');
+  })
+
+  it('Click on the nav on User and url should contain /home/card', () => {
+    cy.contains('User').click();
+    cy.url().should('include', '/home/card')
+  })
+
+  it('Type on input firstName lastName and email and click on Save', () => {
+    cy.visit('http://localhost:4200/home/form');
+    cy.get('.nome')
+      .type('Flavio')
+      .should( 'have.value', 'Flavio');
+    cy.get('.cognome')
+      .type('Fantasia')
+      .should( 'have.value', 'Fantasia');
+    cy.get('.email')
+      .type('email@email.it')
+      .should( 'have.value', 'email@email.it');
+    cy.get('.btn-success').click();
+  })
+})
