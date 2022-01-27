3a6f27dd716aa1ac841db46b84772354097df95a
Ser7ox <fantasiaflavio@gmail.com>
2022-01-27 11:17:13 +0100

    test cypress

--- akita-app/cypress/integration/spec.ts
+++ akita-app/cypress/integration/spec.ts
@@ -6,10 +6,6 @@ describe('My First Test', () => {
     cy.contains('Actions')
   })
 
-  it('Should be true', () => {
-    expect(true).to.equal(true)
-  })
-
   it('Site must contains the term User Card', () => {
     cy.contains('User card')
   })
@@ -27,18 +23,49 @@ describe('My First Test', () => {
     cy.url().should('include', '/home/card')
   })
 
-  it('Go on create user from card, type on input firstName lastName and email and click on Save', () => {
-    cy.visit('/home/table');
-    cy.get('.cypress-btn').click();
+  it('Create user from card, type on input firstName lastName and email and click on Save', () => {
+    cy.visit('/home/form');
     cy.get('.nome')
-      .type('Flavio')
+      .type('Flavio').as('nome')
       .should( 'have.value', 'Flavio');
     cy.get('.cognome')
-      .type('Fantasia')
+      .type('Fantasia').as('cognome')
       .should( 'have.value', 'Fantasia');
-    cy.get('.email')
-      .type('email@email.it')
+    cy.get('.address')
+      .type('email@email.it').as('address')
       .should( 'have.value', 'email@email.it');
+    cy.intercept('POST', 'http://localhost:3000/persone', (req) => {
+      req.body = {
+        check: 'OK',
+      }
+    })
     cy.get('.btn-success').click();
+    cy.request('http://localhost:3000/persone').then(response => {
+      response.body.forEach((element: { check: string }) => {
+        if (element.check === 'OK') {
+          cy.log('Ottimo, check completato. L\'utente creato nel test precedente risulta nel nostro database.');
+        }
+      });
+    })
   })
+
+  it('create user with cy.request', () => {
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3000/persone',
+      body: {
+        "firstName": "Danilo",
+        "lastName": "Merino",
+        "address": "Via dei martiri, 30"
+      }
+    })
+  })
+
+  //it('delete user', () => {
+  //  const id = 3
+  //  cy.request({
+  //    method: 'DELETE',
+  //    url: 'http://localhost:3000/persone/' + id
+  //  })
+  //})
 })
